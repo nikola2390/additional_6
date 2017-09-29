34c8529cb9ed0fb67ef16fffa045cc5dcfeaa1d3
Nikolas <nikolas_23@tut.by>
2017-09-29 22:49:10 +0300

    Zeroes is done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,37 @@
 module.exports = function zeros(expression) {
-  // your solution
+  let arr = expression.split('*');
+	let two = 0;
+	let five = 0;
+	let arr1 = [];
+	let a = 0;
+	for ( let i = 0; i < arr.length; i++ ) {
+		if (arr[i].slice(-2) !== '!!') {
+			a = Number(arr[i].slice(0,-1));
+			let b = a;
+			for (let j = 0; j < b; j++ ) {
+				arr1.push(a);
+				a--;
+			}
+		} else {
+			a = Number(arr[i].slice(0, -2));
+			let b = a;
+			for (let j = 0; j < b/2; j++ ) {
+				arr1.push(a);
+				a-= 2;
+			}
+		}
+	}
+
+	for (let i = 0; i < arr1.length; i++ ) {
+		if (arr1[i] % 2 == 0) {
+			two++;
+		}
+		if (arr1[i] % 5 == 0) {
+			five++;
+		}
+		if (arr1[i] % 25 == 0) {
+			five++;
+		}
+	}
+	return Math.min(two,five);
 }
